daaabc1f36894927fd3af3a64fad172faf54cf48
Bill <whilke@gmail.com>
2015-04-03 22:00:32 -0700

    Server framework for godot client updates

---
+++ Server/routes/godot.js
@@ -0,0 +1,41 @@
+var GameServer = require("../gameServer/server.js");
+var battleServer = GameServer.Instance();
+
+var express = require('express');
+var router = express.Router();
+
+router.get("/get_token/:pass", function(req, res) {
+    
+    var pass = req.params.pass;
+    
+    
+    var token_str =  battleServer.get_token(pass);
+    var token = {
+        token :  token_str,
+        fetch_url : "http://GAMESERVER:8080/headless/world_state/" + token_str
+    }; 
+        
+    res.setHeader('Content-Type:', 'application/json');
+    res.end( JSON.stringify( token ) );
+});
+
+router.get("/world_state/:token", function (req, res) {
+   
+    var token = req.params.token;
+    var isValid = battleServer.is_token_valid(token);
+     
+    res.setHeader('Content-Type:', 'application/json');
+     
+    if (!isValid)
+    {
+       res.end(JSON.stringify({status:"error"}));
+    }
+    else
+    {
+        var world_state = battleServer.get_world_state();
+        res.end(JSON.stringify(world_state));
+    }
+});
+
+
+module.exports = router;
