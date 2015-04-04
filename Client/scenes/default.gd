cd52ba7766271d84a9ef72defa55c1539fb2ac7f
Bill Hilke <whilke@gmail.com>
2015-04-03 21:02:41 -0700

    first cut at getting world state from server.

--- Client/scenes/default.gd
+++ Client/scenes/default.gd
@@ -3,18 +3,41 @@ extends Node2D
 
 var webClient = preload("res://scripts/httpClient.gd")
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+var labelNode
+var timerNode
+
+var web
+var update_url
 
 func _ready():
 	# Initialization here
-	var web = webClient.new()
-		
-	web.get("http://www.enbask.com", self, "_on_get")
+	
+	labelNode = get_node("Label");
+	timerNode = get_node("Timer")
+	
+	web = webClient.new()
+	web.get("http://GAMESERVER:8080/headless/get_token/password", self, "on_init_server")
 
-func _on_get(data):
-	print(data)
+func on_init_server(data):
+	var d = {}
+	var err = d.parse_json(data)
+	if (err == OK):
+		update_url = d["fetch_url"];
+		timerNode.start()
 	
 
 
+
+
+func _on_Timer_timeout():
+	timerNode.stop()
+	web = webClient.new()
+	web.get(update_url, self, "on_fetch_server")
+	
+func on_fetch_server(data):
+	var d = {}
+	var err = d.parse_json(data)
+	if (err == OK):
+		labelNode.set_text("Status: " + d["status"] + "\r\n" + "Time:" + str(d["time"]));
+	
+	timerNode.start()
